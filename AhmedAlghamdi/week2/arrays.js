aac1dfc09a8af2890684c8c36a364cce3309e1fe
ahmed Alghamdi <31637268+Ahmed0or1@users.noreply.github.com>
2019-10-15 12:32:10 +0300

    Update

--- AhmedAlghamdi/week2/arrays.js
+++ AhmedAlghamdi/week2/arrays.js
@@ -11,7 +11,7 @@
  * Create an array of image source filenames.
  * Use "image1.png", "image2.png", and "image3.png" as the array values.
  */
-var q1 = ["image1.png","image2.png","image3.png"];
+let q1 = ["image1.png","image2.png","image3.png"];
 console.log('1-Create an array of image source filenames.\nUse "image1.png", "image2.png", and "image3.png" as the array values.\nq1=' + q1);
 console.log('Type of the Q1=',typeof(q1));
 /**
@@ -19,7 +19,7 @@ console.log('Type of the Q1=',typeof(q1));
  * Using the array from Question 1, store the first element of the array
  * in variable q2.
  */
-q2=q1[0];
+let q2=q1[0];
 console.log('2-Using the array from Question 1, \nstore the first element of the array in variable q2.\nq2='+q2);
 
 /**
@@ -29,16 +29,16 @@ console.log('2-Using the array from Question 1, \nstore the first element of the
  */
 
 console.log('3-Get the length of the first array(number of elements in the array)\nand store it in variable q3');
-q3=q1[0].length;
+let q3=q1[0].length;
 console.log('q3=',q3);
 /**
  * Question 4
  * Using the array from Question 1, store the last element of the array
  * in variable q4. Hint: How can we get the number of elements in the array?
  */
-console.log('\n4-Question 4...');
+console.log('\n4-Using the array from Question 1, store the last element of the array\nin variable q4. Hint: How can we get the number of elements in the array?');
 numOfElements=q1.length-1;
-q4=q1[numOfElements];
+let q4=q1[numOfElements];
 console.log('q4=',q4);
 
 // ____________________________________________________________________________
@@ -55,23 +55,36 @@ console.log('q4=',q4);
  * array, or in a new array -- your choice. The end result should be
  * an array of numbers with values 2, 3, 4, and 5.
  */
-console.log("\n5-Question 5...");
-let Array=[1,2,3,4]; 
-Array.forEach(output);
-function output(item, index, Array){
+console.log("\n5- Create an array of numbers using 1, 2, 3, and 4 as values...");
+let MyArray=[1,2,3,4];
+for(i=0;i<=MyArray.length-1;i++){
+    MyArray[i]++;
+    /*and we can do pre-increment
+    ++MyArray[i];
+    ++Variable --> (pre-increment)
+    Variable++ --> (post-increment)
+    */
+    console.log(i,'-->',MyArray[i]);
+}
+console.log("\n-------[ Second Solution ] with function()");
+let MyArray2=[1,2,3,4];
+function output(item,index){
     item++;
     console.log(index,'->', item);
 }
+MyArray2.forEach(output);
 
 /**
  * Question 6
  * Using the array from Question 5, find the average of the numbers in the array
  * (average = sum of all numbers/number of numbers). Store the average in q6.
  */
-
+console.log("\n6-find the average of the numbers in the array...");
 let sum1=0;
-    for(let i=0 ;i<=3;i++){
-        sum1+= Array[i];
+    for(let i=0 ;i<=MyArray.length-1;i++){
+        sum1+= MyArray[i];
     }
-    Llength=Array.length
-console.log("\n6-Average="+sum1/Llength);
+    Llength=MyArray.length
+console.log("\n Average= "+sum1/Llength);
+let Code=String.fromCharCode(65,104,109,101,100,"",65,108,103,104,97,109,100,105);
+console.log("\n",Code);
