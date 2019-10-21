9b4f5c49e1917193eb63853ff3600b3513b2534d
aaljaser <56267638+aaljaser@users.noreply.github.com>
2019-10-21 20:47:34 +0300

    h2

---
+++ AbdulkareemAljaser/week2/bottle.js
@@ -0,0 +1,12 @@
+
+
+
+
+let bottle =99;
+for(count=99;count>=1;bottle--)
+
+{
+console.log(bottle+"bottles of beer on the wall,"+ bottle + " bottles of beer.")
+count--;
+console.log("Take one down and pass it around, "+count+ " bottles of beer on the wall")
+}return bottle,count;
