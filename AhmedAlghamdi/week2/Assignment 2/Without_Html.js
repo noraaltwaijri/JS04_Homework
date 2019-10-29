8cf7a52eb27225f5a08f2f80f61c25324ee7d128
ahmed Alghamdi <31637268+Ahmed0or1@users.noreply.github.com>
2019-10-29 23:15:09 +0300

    Week3
    Comfort level = 5/4
    completeness = 5/4
    
    Note: :+1:

--- AhmedAlghamdi/week2/Assignment 2/Without_Html.js
+++ AhmedAlghamdi/week2/Assignment 2/Without_Html.js
@@ -17,4 +17,19 @@ function Addresses () {
 
 let Code=String.fromCharCode(65,104,109,101,100,"",65,108,103,104,97,109,100,105);
 document.write(`<br><p>${Code}</p><br>`);
-*/
+
+var streetNumber = ['9377','140','919','9479','30','584'];
+var streetName = ['Sheffield Dr.','Inverness St.','Golden Star Dr.','E. Ridge Circle','Pierce St.','University St.'];
+var cityName = ['Lake Mary','Valrico','Augusta','Williamsport','New Lenox','Kissimmee'];
+var stateName = ['FL','UT','GA','PA','IL','AL'];
+var zipCode = ['32746','32746','32746','17701','60451','34741'];
+
+function getRandom (input){
+    return input[Math.floor ((Math.random()*input.length))];
+}
+
+function createAdress (){
+    return `${getRandom(streetNumber)} ${getRandom(streetName)} ${getRandom(cityName)} ${getRandom(stateName)} ${getRandom(zipCode)}`;
+}
+var address = createAdress();
+console.log(address);*/
