47ee67ad64cdae98c5efa7052e693dc067b7874f
Jafar <jaafar.abdullah.1414@gmail.com>
2019-11-03 15:11:54 +0300

    LV

--- AbdulrazzaqMaihoub/week2/homework1week2.js
+++
@@ -1,9 +0,0 @@
-var beernum=100;
-while(beernum>1 && beernum<=100)
-{
- console.log(beernum +' bottle of beer on the wall,'+ (--beernum) +" bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.");
-
-}
-
-console.log('No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.');
-beernum--;
